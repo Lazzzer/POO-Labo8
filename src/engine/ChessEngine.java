3a19d6b3ac5edb8a5c8af61d525671febfb2fd54
Lazzzer <lazarpavicevic@protonmail.com>
2021-12-30 19:42:06 +0100

    refactor(): clean up newGame() and handle turns

--- src/engine/ChessEngine.java
+++ src/engine/ChessEngine.java
@@ -8,8 +8,62 @@ import engine.piece.*;
 
 public class ChessEngine implements ChessController {
     private ChessView view;
+
     final int boardSize = 8;
-    Piece[][] board = new Piece[boardSize][boardSize];
+    Piece[][] board;
+    PlayerColor turn;
+
+    private void populateBoard() {
+        int position = 0;
+
+        // Tours
+        board[0][position] = new Rook(PlayerColor.WHITE);
+        board[0][boardSize - 1 - position] = new Rook(PlayerColor.WHITE);
+        board[boardSize - 1][position] = new Rook(PlayerColor.BLACK);
+        board[boardSize - 1][boardSize - 1 - position] = new Rook(PlayerColor.BLACK);
+
+        position++;
+        // Cavaliers
+        board[0][position] = new Knight(PlayerColor.WHITE);
+        board[0][boardSize - 1 - position] = new Knight(PlayerColor.WHITE);
+        board[boardSize - 1][position] = new Knight(PlayerColor.BLACK);
+        board[boardSize - 1][boardSize - 1 - position] = new Knight(PlayerColor.BLACK);
+
+        position++;
+        // Fous
+        board[0][position] = new Bishop(PlayerColor.WHITE);
+        board[0][boardSize - 1 - position] = new Bishop(PlayerColor.WHITE);
+        board[boardSize - 1][position] = new Bishop(PlayerColor.BLACK);
+        board[boardSize - 1][boardSize - 1 - position] = new Bishop(PlayerColor.BLACK);
+
+        position++;
+        // Reines et Rois
+        board[0][position] = new Queen(PlayerColor.WHITE);
+        board[0][boardSize - 1 - position] = new King(PlayerColor.WHITE);
+        board[boardSize - 1][position] = new Queen(PlayerColor.BLACK);
+        board[boardSize - 1][boardSize - 1 - position] = new King(PlayerColor.BLACK);
+
+        // Pions
+        for (int i = 0; i < boardSize; ++i) {
+            board[1][i] = new Pawn(PlayerColor.WHITE);
+            board[boardSize - 2][i] = new Pawn(PlayerColor.BLACK);
+        }
+    }
+
+    private void drawBoard() {
+        for (int i = 0; i < boardSize; ++i) {
+            for (int j = 0; j < boardSize; ++j) {
+                if (board[i][j] != null) {
+                    view.putPiece(board[i][j].getPieceType(), board[i][j].getColor(), j, i);
+                }
+            }
+        }
+    }
+
+    private void switchTurn() {
+        turn = turn == PlayerColor.WHITE ? PlayerColor.BLACK : PlayerColor.WHITE;
+    }
+
     @Override
     public void start(ChessView view) {
         this.view = view;
@@ -18,82 +72,25 @@ public class ChessEngine implements ChessController {
 
     @Override
     public boolean move(int fromX, int fromY, int toX, int toY) {
-        if(board[fromY][fromX] == null || (board[toY][toX] != null && board[fromY][fromX].getColor() == board[toY][toX].getColor())){
+        if (board[fromY][fromX] == null || board[fromY][fromX].getColor() != turn || (board[toY][toX] != null && board[fromY][fromX].getColor() == board[toY][toX].getColor())) {
             return false;
-        } else if (board[fromY][fromX].move(board, fromX, fromY, toX, toY)){
-            view.removePiece(fromX,fromY);
-            view.removePiece(toX,toY);
-            view.putPiece(board[fromY][fromX].getPieceType(),board[fromY][fromX].getColor(),toX,toY);
+        } else if (board[fromY][fromX].move(board, fromX, fromY, toX, toY)) {
+            view.removePiece(fromX, fromY);
+            view.removePiece(toX, toY);
+            view.putPiece(board[fromY][fromX].getPieceType(), board[fromY][fromX].getColor(), toX, toY);
             board[toY][toX] = board[fromY][fromX];
             board[fromY][fromX] = null;
-    
+            switchTurn();
+            return true;
         }
         return false;
     }
-    
+
     @Override
     public void newGame() {
-        int position = 0;
-    
-        // Pour les Tours
-        //Blanches
-        board[0][position] = new Rook(PlayerColor.WHITE);
-        view.putPiece(PieceType.ROOK,PlayerColor.WHITE,position,0);
-        board[0][boardSize -1-position] = new Rook(PlayerColor.WHITE);
-        view.putPiece(PieceType.ROOK,PlayerColor.WHITE, boardSize -1 - position,0);
-        //Noires
-        board[boardSize -1][position] = new Rook(PlayerColor.BLACK);
-        view.putPiece(PieceType.ROOK,PlayerColor.BLACK,position, boardSize -1);
-        board[boardSize -1][boardSize -1-position] = new Rook(PlayerColor.BLACK);
-        view.putPiece(PieceType.ROOK,PlayerColor.BLACK, boardSize -1-position, boardSize -1);
-    
-        position++;
-        // Pour les Cavaliers
-        //Blanches
-        board[0][position] = new Knight(PlayerColor.WHITE);
-        view.putPiece(PieceType.KNIGHT,PlayerColor.WHITE,position,0);
-        board[0][boardSize -1-position] = new Knight(PlayerColor.WHITE);
-        view.putPiece(PieceType.KNIGHT,PlayerColor.WHITE, boardSize -1 - position,0);
-        //Noires
-        board[boardSize -1][position] = new Knight(PlayerColor.BLACK);
-        view.putPiece(PieceType.KNIGHT,PlayerColor.BLACK,position, boardSize -1);
-        board[boardSize -1][boardSize -1-position] = new Knight(PlayerColor.BLACK);
-        view.putPiece(PieceType.KNIGHT,PlayerColor.BLACK, boardSize -1 - position, boardSize -1);
-    
-        position++;
-        // Pour les Fous
-        //Blanches
-        board[0][position] = new Bishop(PlayerColor.WHITE);
-        view.putPiece(PieceType.BISHOP,PlayerColor.WHITE,position,0);
-        board[0][boardSize -1-position] = new Bishop(PlayerColor.WHITE);
-        view.putPiece(PieceType.BISHOP,PlayerColor.WHITE, boardSize -1 - position,0);
-        //Noires
-        board[boardSize -1][position] = new Bishop(PlayerColor.BLACK);
-        view.putPiece(PieceType.BISHOP,PlayerColor.BLACK,position, boardSize -1);
-        board[boardSize -1][boardSize -1-position] = new Bishop(PlayerColor.BLACK);
-        view.putPiece(PieceType.BISHOP,PlayerColor.BLACK, boardSize -1 - position, boardSize -1);
-    
-        position++;
-        // Pour les Spéciales
-        //Blanches
-        board[0][position] = new Queen(PlayerColor.WHITE);
-        view.putPiece(PieceType.QUEEN,PlayerColor.WHITE,position,0);
-        board[0][boardSize -1-position] = new King(PlayerColor.WHITE);
-        view.putPiece(PieceType.KING,PlayerColor.WHITE, boardSize -1-position,0);
-        //Noires
-        board[boardSize -1][position] = new Queen(PlayerColor.BLACK);
-        view.putPiece(PieceType.QUEEN,PlayerColor.BLACK,position, boardSize -1);
-        board[boardSize -1][boardSize -1-position] = new King(PlayerColor.BLACK);
-        view.putPiece(PieceType.KING,PlayerColor.BLACK, boardSize -1-position, boardSize -1);
-        
-        for (int i = 0; i < boardSize; ++i){
-            // Pour les pions blancs
-            board[1][i] = new Pawn(PlayerColor.WHITE);
-            view.putPiece(PieceType.PAWN,PlayerColor.WHITE,i,1);
-            
-            // Pour les pions noirs
-            board[boardSize -2][i] = new Pawn(PlayerColor.BLACK);
-            view.putPiece(PieceType.PAWN,PlayerColor.BLACK,i, boardSize -2);
-        }
+        board = new Piece[boardSize][boardSize];
+        populateBoard();
+        drawBoard();
+        turn = PlayerColor.WHITE;
     }
 }
